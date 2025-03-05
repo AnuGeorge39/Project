ffa89f1d0f0c010fbbea7465ea052bd802a471a5
Anu7380 <105626769+Anu7380@users.noreply.github.com>
2025-03-06 00:34:26 +0530

    navbarcss

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import "../css/NavBar.css";
 
 function CollapsibleExample() {
   return (
@@ -25,28 +26,37 @@ function CollapsibleExample() {
               <NavDropdown.Item href="#action/3.3">Branding</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Advertising</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Package Design</NavDropdown.Item>
-              
             </NavDropdown>
-            <Nav.Link href="#features">WHAT</Nav.Link>
-            <Nav.Link href="#pricing">WE</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+           
+            <NavDropdown title="WE" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Ariussoft Infotech Pvt.Ltd</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2"> Arius Web Studio</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Think Thick</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Host Station</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Core Team</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Testimonials</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Insights</NavDropdown.Item>
             </NavDropdown>
+            
+            <NavDropdown title="DO" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Portfolio</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Gallery</NavDropdown.Item>
+            </NavDropdown>
+            
+            <NavDropdown title="FOR" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Clients</NavDropdown.Item>
+            </NavDropdown>
+            
+            <NavDropdown title="YOU" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Contact Us</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Feedback</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Get a Quote</NavDropdown.Item>
+            </NavDropdown>
+          
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
+            <Nav.Link href="#deets">Get a Quote</Nav.Link>
+            </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
