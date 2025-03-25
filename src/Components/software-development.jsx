92c541434c95b751f626c0ac14abedb5c94e5e98
Anu <anugeorge05472gmail.com>
2025-03-25 02:17:32 -0700

    "backend"

---
+++ src/Components/software-development.jsx
@@ -0,0 +1,55 @@
+// In UncontrolledExample.js
+import React from 'react';
+import { Carousel, Image } from 'react-bootstrap';
+import Slide_1 from '../assets/Images/img1.jpg';
+import Slide_2 from '../assets/Images/img2.jpg';
+import Slide_3 from '../assets/Images/img3.jpg';
+import Slide_4 from '../assets/Images/img4.jpg';
+import Slide_5 from '../assets/Images/img5.jpg';
+import "./Carousel.css"
+
+function SoftwareDevelopment() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <Image className='slide_img' src={Slide_1} />
+        <Carousel.Caption>
+          <h3>WebSite Development</h3>
+          <p> Process of building, programming, and maintaining websites and web applications.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className='slide_img' src={Slide_2} />
+        <Carousel.Caption>
+          <h3>Web Development</h3>
+          <p>There are three main types of web development: front-end development, back-end development, and full stack development..</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className='slide_img' src={Slide_3} />
+        <Carousel.Caption>
+          <h3>Digital marketing </h3>
+          <p>Digital marketing is the component of marketing that uses the Internet and online-based digital technologies such as desktop computers, mobile phones,
+             and other digital media and platforms to promote products and services.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className='slide_img' src={Slide_4} />
+        <Carousel.Caption>
+          <h3>Web Design</h3>
+          <p>Web design involves creating the visual layout and aesthetics of a website, focusing on user experience, graphics, and overall look. Front-end development, on the other hand, involves implementing the design into a functional website using coding languages like HTML, CSS, and JavaScript.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Image className='slide_img' src={Slide_5} />
+        <Carousel.Caption>
+          <h3>Web Development</h3>
+          <p>Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services..</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+     
+    </Carousel>
+  );
+}
+
+export default SoftwareDevelopment;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import UncontrolledExample from "../Components/Carousel.jsx";
+import About_us from '../Components/About_us.jsx';
+import Services from '../Components/services.jsx';
+
+export default function Home() {
+  return (
+    <div>
+      
+        <UncontrolledExample/>
+        <About_us/>
+        <Services/>
+        
+        </div>
+  )
+}
