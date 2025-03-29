638174f586c5ab5dd1ad7b1049c51f5ca9fdbb55
Anu <anugeorge05472gmail.com>
2025-03-29 14:48:22 -0700

    Add reusable card components for UI enhancement

--- frontend/src/Components/Cards.jsx
+++ frontend/src/Components/Cards.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Cards() {
-  return (
-    <div>Cards</div>
-  )
-}
+export default function Cards ({ name, email, message })  {
+      return (
+        <div className="card mt-3">
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{email}</h6>
+            <p className="card-text">{message}</p>
+          </div>
+        </div>
+      );
+    };
+    
--- frontend/src/Components/software-development.jsx
+++ frontend/src/Components/software-development.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import '../css/Banner.css'
+import Cards from "../Components/Cards.jsx"
 import BannerImage from '../assets/Images/img1.jpg'; // Choose your banner image
 
 function SoftwareDevelopment() {
   return (
-    <div className="banner">
+    <>
+     <div className="banner">
       <div className="banner-content">
         <h1>Software Development</h1>
         <p>Software development is the process of designing, creating, testing, and maintaining software applications to meet specific needs. It involves writing code, debugging errors, and improving functionality over time..</p>
       </div>
     </div>
+    <Cards
+    name =" anu"
+    mail= "anu@gmail.com"
+    message =" hi hope you are fine "/>
+    e
+    </>
+   
   );
 }
 
